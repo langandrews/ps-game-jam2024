feeaf345ef1718d22d4553208a8cb2c5f3a1f06d
Luke Justice <justicelu97@gmail.com>
2024-07-17 21:53:29 -0400

    adding minimum jump height

--- scenes/objects/player_movement.gd
+++ scenes/objects/player_movement.gd
@@ -1,16 +1,17 @@
 extends CharacterBody2D
 
-@export var max_horizontal_speed = 400
-@export var max_vertical_speed = 1000
-@export var acceleration = 25
-@export var friction_factor = 0.85
+@export var max_horizontal_speed = 500
+@export var max_vertical_speed = 5000
+@export var acceleration = 50
+@export var friction_factor = 0.8
 @export var gravity = 20
-@export var extra_gravity_multiplier = 1.5
+@export var extra_gravity_multiplier = 2
 
-@export var jump_velocity = 1000
+@export var jump_velocity = 900
 
 var last_ground_touch_frame = -100000
 var buffered_jump_frame = -100000
+var jump_start_frame = -100000
 
 func _physics_process(delta):
 	update_coyote()
@@ -23,15 +24,14 @@ func _physics_process(delta):
 	if abs(input_horizontal) < 0.01 and is_on_floor():
 		velocity.x *= friction_factor 
 	
-	
 	if Input.is_action_just_pressed("jump"):
 		if can_jump():
 			jump()
 		else:
 			buffer_jump()
 	elif is_buffered_jump() and can_jump():
-		jump()	
-	elif not Input.is_action_pressed("jump") and velocity.y < 0:
+		jump()
+	elif not Input.is_action_pressed("jump") and velocity.y < 0 and not is_min_jump_window():
 		velocity.y *= 0.1
 	
 	velocity.x = clamp(velocity.x, -max_horizontal_speed, max_horizontal_speed)
@@ -40,6 +40,7 @@ func _physics_process(delta):
 
 func jump():
 	velocity.y = -jump_velocity
+	jump_start_frame = Engine.get_physics_frames()
 	last_ground_touch_frame = -100000
 	buffered_jump_frame = -1000000
 
@@ -58,3 +59,6 @@ func is_buffered_jump() -> bool:
 
 func buffer_jump():
 	buffered_jump_frame = Engine.get_physics_frames()
+
+func is_min_jump_window():
+	return Engine.get_physics_frames() - jump_start_frame < 6
