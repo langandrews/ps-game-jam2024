4e65407208788cc8dd0838286ca8b1b4b170a918
Luke Justice <justicelu97@gmail.com>
2024-07-17 21:45:05 -0400

    adding coyote and buffered jumps

--- scenes/objects/player_movement.gd
+++ scenes/objects/player_movement.gd
@@ -9,8 +9,12 @@ extends CharacterBody2D
 
 @export var jump_velocity = 1000
 
+var last_ground_touch_frame = -100000
+var buffered_jump_frame = -100000
 
 func _physics_process(delta):
+	update_coyote()
+	
 	velocity.y += gravity * extra_gravity_multiplier if velocity.y else 1.0
 	
 	var input_horizontal := Input.get_axis("move_left", "move_right")
@@ -19,11 +23,38 @@ func _physics_process(delta):
 	if abs(input_horizontal) < 0.01 and is_on_floor():
 		velocity.x *= friction_factor 
 	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = -jump_velocity
-	elif Input.is_action_just_released("jump") and velocity.y < 0:
+	
+	if Input.is_action_just_pressed("jump"):
+		if can_jump():
+			jump()
+		else:
+			buffer_jump()
+	elif is_buffered_jump() and can_jump():
+		jump()	
+	elif not Input.is_action_pressed("jump") and velocity.y < 0:
 		velocity.y *= 0.1
 	
 	velocity.x = clamp(velocity.x, -max_horizontal_speed, max_horizontal_speed)
 	velocity.y = clamp(velocity.y, -max_vertical_speed, max_vertical_speed)
 	move_and_slide()
+
+func jump():
+	velocity.y = -jump_velocity
+	last_ground_touch_frame = -100000
+	buffered_jump_frame = -1000000
+
+func can_jump() -> bool:
+	return is_coyote() || is_on_floor()
+
+func is_coyote() -> bool:
+	return Engine.get_physics_frames() - last_ground_touch_frame < 10
+
+func update_coyote():
+	if is_on_floor():
+		last_ground_touch_frame = Engine.get_physics_frames()
+
+func is_buffered_jump() -> bool:
+	return Engine.get_physics_frames() - buffered_jump_frame < 15
+
+func buffer_jump():
+	buffered_jump_frame = Engine.get_physics_frames()
