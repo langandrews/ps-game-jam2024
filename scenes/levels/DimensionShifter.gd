9b50abb60edfa539831a52f194eab6dbdb990b18
Luke Justice <justicelu97@gmail.com>
2024-07-20 19:53:31 -0400

    preventing swap when on a tile that is blocked

--- scenes/levels/DimensionShifter.gd
+++ scenes/levels/DimensionShifter.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @export var offset = 400
 @onready var player = $"../Player"
+@onready var ground_tiles = $"../Ground Tiles"
 
 var isCurrnetDark = false
 
@@ -9,10 +10,20 @@ func _ready():
 	SignalBus.OnSwap.connect(swap)
 
 func _physics_process(_delta):
-	if Input.is_action_just_pressed("swap_dimensions"):
-		SignalBus.OnSwap.emit(!isCurrnetDark)
+	if not Input.is_action_just_pressed("swap_dimensions"): return
+	
+	var testPosition = player.position
+	testPosition.y += get_offset(!isCurrnetDark)
+	var tilePos = ground_tiles.local_to_map(testPosition)
+	var tile = ground_tiles.get_cell_tile_data(1, tilePos)
+	if tile: return
+	
+	SignalBus.OnSwap.emit(!isCurrnetDark)
 
 func swap(isDark: bool):
 	isCurrnetDark = isDark
-	player.position.y += offset if isDark else -offset
+	player.position.y += get_offset(isDark)
+
+func get_offset(isDark: bool) -> int:
+	return offset if isDark else -offset
 	
