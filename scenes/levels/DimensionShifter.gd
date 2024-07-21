95f6117795381b71784e8a89921d9e66a618ea28
Luke Justice <justicelu97@gmail.com>
2024-07-20 20:21:30 -0400

    making swap more user friendly

--- scenes/levels/DimensionShifter.gd
+++ scenes/levels/DimensionShifter.gd
@@ -5,6 +5,13 @@ extends Node2D
 @onready var ground_tiles = $"../Ground Tiles"
 
 var isCurrnetDark = false
+var offsets := [
+	Vector2(0, 0),
+	Vector2(4, 0),
+	Vector2(-4, 0),
+	Vector2(0, 4),
+	Vector2(0, -4),
+]
 
 func _ready():
 	SignalBus.OnSwap.connect(swap)
@@ -13,12 +20,16 @@ func _physics_process(_delta):
 	if not Input.is_action_just_pressed("swap_dimensions"): return
 	
 	var testPosition = player.position
-	testPosition.y += get_offset(!isCurrnetDark)
-	var tilePos = ground_tiles.local_to_map(testPosition)
-	var tile = ground_tiles.get_cell_tile_data(1, tilePos)
-	if tile: return
+	testPosition.y += get_offset(!isCurrnetDark) - 8
+	for offset in offsets:
+		var tilePos = ground_tiles.local_to_map(testPosition + offset)
+		var tile = ground_tiles.get_cell_tile_data(1, tilePos)
+		var isEmpty = tile == null
+		if isEmpty:
+			SignalBus.OnSwap.emit(!isCurrnetDark)
+			return
 	
-	SignalBus.OnSwap.emit(!isCurrnetDark)
+	
 
 func swap(isDark: bool):
 	isCurrnetDark = isDark
