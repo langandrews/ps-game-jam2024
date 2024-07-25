b5e5a63d9bb0914783b82383a8f607b2475a63d3
Luke Justice <justicelu97@gmail.com>
2024-07-24 22:48:47 -0400

    fixing inconsistency with spikes

--- scenes/objects/spikes/spikes.gd
+++ scenes/objects/spikes/spikes.gd
@@ -1,13 +1,8 @@
 extends Area2D
 
 func _on_area_entered(area):
-	print("body entered spikes")
 	var body = area.get_parent()
-	print(body.velocity.y)
-	print(body.name)
-	if body is PlayerMovement and body.velocity.y > 0:
+	if body is PlayerMovement and body.get_real_velocity().y > 0:
 		for child in area.get_children():
-			print("child")
-			print(str(child))
 			if child is PlayerDeath:
 				child.death()
