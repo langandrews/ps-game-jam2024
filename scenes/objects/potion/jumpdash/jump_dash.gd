33c8cf3a66e3dcc6f16ecc495dc3baa987b3164d
Luke Justice <justicelu97@gmail.com>
2024-07-30 00:16:55 -0400

    adding inverted jump dash for design purposes

--- scenes/objects/potion/jumpdash/jump_dash.gd
+++ scenes/objects/potion/jumpdash/jump_dash.gd
@@ -2,13 +2,15 @@ extends Potion
 
 const DUST = preload("res://scenes/objects/potion/jumpdash/dust.tscn")
 
+@export var is_dark_dash := true
+
 func use(user: Node2D):
 	var vfx := DUST.instantiate()
 	vfx.position = user.position + Vector2(0, -8)
 	vfx.emitting = true
 	user.get_parent().add_child(vfx)
 	
-	if is_dark:
+	if is_dark == is_dark_dash:
 		SignalBus.Dash.emit()
 		if user is CharacterBody2D:
 			vfx.rotate(-PI / 2.0 if user.get_real_velocity().x < 0 else PI / 2.0)
